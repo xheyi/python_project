0fa39c2a2b778b34367e6c2a3a0ae6dd687ff142
Awojobi Suleiman <awojobisuleiman1@gmail.com>
2024-09-26 18:01:44 +0100

    this is the answer for total value of product per company in automation project

--- test.py
+++ test.py
@@ -4,16 +4,26 @@ inv_file = openpyxl.load_workbook("inventory.xlsx")
 product_list = inv_file["Sheet1"]
 
 product_per_company = {}
-
+total_product_value_per_company = {}
 for product_row in range(2, product_list.max_row + 1):
     company_name = product_list.cell(product_row, 4).value
+    inventory = product_list.cell(product_row, 2).value
+    price = product_list.cell(product_row, 3).value
 
     if company_name in product_per_company:
         current_amount = product_per_company.get(company_name)
+
         product_per_company[company_name] = current_amount + 1
 
     else:
         product_per_company[company_name] = 1
 
-print(product_per_company)
+    if company_name in total_product_value_per_company:
+        current_total = total_product_value_per_company.get(company_name)
+        total_product_value_per_company[company_name] = current_total + inventory * price
 
+    else:
+        total_product_value_per_company[company_name] = inventory * price
+
+print(product_per_company)
+print(total_product_value_per_company)
